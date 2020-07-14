f4784dd37789dc6de95447a8ce5901e6cb31a3cb
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-14 02:30:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
-  if array.length
+  if array.length == 1
     array.join
-  elsif 2
+  elsif array.length == 2
     array.join(" and ")
+  elsif array.length == 3
+  
   end
 end
