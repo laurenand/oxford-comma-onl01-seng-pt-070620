cc642d2bd1a2c23e8d649ff90ea5eeb0facf54f6
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-14 01:57:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     array.join
   when 2
     array.join(" and ")
-  when ["kiwi", "durian", "starfruit"]
-    return "kiwi, durian, and starfruit"
+  when 3
+    array
   end
 end
